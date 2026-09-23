bc58c7192a657749d7a65854d318cd2e6988ff35
mrts-wx <mrts.wx@icloud.com>
2026-09-22 21:15:59 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Playing Status", // required by Discord API, not shown in the client
         state: "Clash Of Clans 🛡️",     // this is what people actually see
         type: 0,               // Custom
       },
