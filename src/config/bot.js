861eb1445b955ec85991f97828a0153b78d16341
mrts-wx <mrts.wx@icloud.com>
2026-09-22 21:13:40 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        state: "Clash Of Clans 🛡️",     // this is what people actually see
+        type: 0,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#ff4400",
+      secondary: "#212020",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
